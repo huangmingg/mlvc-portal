0bb44c49f2243dc9febac0b724288511cf34b673
davidwang98 <work.davidwang@gmail.com>
2021-07-22 18:41:29 +0800

    "some basic html css"
    This reverts commit cb45597449dda4f90bbf2ca99388bf65a2ef2a40.

--- src/environments/environment.default.js
+++ src/environments/environment.default.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
 // const mlvcServiceEndpoint = 'https://mlvc-service.herokuapp.com';
 const mlvcServiceEndpoint = 'http://127.0.0.1:5000';
 
+
 export { 
   firebaseConfig, 
   mlvcServiceEndpoint, 
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -4,55 +4,7 @@
       fluid
       class="p-4 bg-light"
     >
-      <div class="container100">
-        <div class="container50">
-          <div>
-            <h2 style="margin-bottom: 10px;">About Project</h2>
-            <p>The MLVC project aims to provide insight into the Startup & Venture Capital industry<br/>
-              ++what the application does
-            </p>
-          </div>
-          <br/>
-          <div>
-            <h2 style="margin-bottom: 10px;">Motivations of Project</h2>
-            <p>Something</p>
-          </div>
-          <br/>
-          <div>
-            <h2 style="margin-bottom: 10px;">Statistics of Data</h2>
-            <p>Something</p>
-          </div>
-        </div>
-        <div class="container50">
-          <div>
-            <h2 style="margin-bottom: 10px;">Team Members</h2>
-            <p>
-              <b>Team Lead:</b> Huang Ming<br/>
-              <b>Data:</b> David and Jamais<br/>
-              <b>Feature Engineering:</b> Kexin and David<br/>
-              <b>Supervised Learning:</b> Eram<br/>
-              <b>Unsupervised Learning:</b> Daniel<br/>
-              <b>Visualisation:</b> Jamais and Kexin
-            </p>
-          </div>
-          <br/>
-          <div>
-            <h2 style="margin-bottom: 10px;">FAQ</h2>
-            <div>
-              <b>How do I use the toilet?</b>
-              <p>Find one, and go</p>
-            </div>
-            <div>
-              <b>How do I use the toilet?</b>
-              <p>Find one, and go</p>
-            </div>
-            <div>
-              <b>How do I use the toilet?</b>
-              <p>Find one, and go</p>
-            </div>
-          </div>
-        </div>
-      </div>
+      <div>Home Page</div>
     </b-container>
   </div>
 </template>
@@ -74,17 +26,5 @@ export default {
 </script>
 
 <style scoped>
-
-.container100 {
-  display: block;
-  width: 100%;
-  overflow: auto;
-}
-
-.container50 {
-  display: block;
-  width: 50%;
-  float: left;
-}
 
 </style>
